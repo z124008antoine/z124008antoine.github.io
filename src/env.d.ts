efda6a6b9ff1cf0fcb7b5a964656d5a8ff9046e4
Antoine Blumenroeder <z124008@shibaura-it.ac.jp>
2024-04-27 13:19:58 +1000

    working transitions (kindof)

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,9 @@
 // import gsap type
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis/types";
 interface Window { 
+    lenis: typeof Lenis;
     gsap: typeof gsap;
     ScrollTrigger: typeof ScrollTrigger;
 }
--- src/scripts/transitions.ts
+++ src/scripts/transitions.ts
@@ -17,3 +17,38 @@ export const textSpacing = {
     forwards: textAnim,
     backwards: textAnim,
 };
+
+const slideAnim = (duration: number, toRight: boolean) => {
+    return {
+        forwards: {
+            old: [{
+                name: "slideTo" + (toRight ? "Right" : "Left"),
+                duration,
+                easing: "cubic-bezier(0.76, 0, 0.24, 1)",
+                fillMode: "both"
+            }],
+            new: [{
+                name: "slideFrom" + (toRight ? "Left" : "Right"),
+                duration,
+                easing: "cubic-bezier(0.76, 0, 0.24, 1)",
+                fillMode: "both"
+            }]
+        },
+        backwards: {
+            old: [
+                { "name": "slideTo" + (toRight ? "Left" : "Right") }
+            ],
+            new: [
+                { "name": "slideFrom" + (toRight ? "Right" : "Left") }
+            ]
+        }
+    };
+}
+
+export const slideCustom = (duration: number, direction = 'left') => {
+    if (direction === 'right')
+        return slideAnim(duration, true);
+    else if (direction === 'left')
+        return slideAnim(duration, false);
+    else throw new Error('Invalid direction');
+};
