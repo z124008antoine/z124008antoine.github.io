30f4c3e62c5709de1c2f5426b37c04363888bed5
Antoine Blumenroeder <z124008@shibaura-it.ac.jp>
2024-04-27 15:30:56 +1000

    cleanup

--- src/scripts/transitions.ts
+++ src/scripts/transitions.ts
@@ -1,23 +1,3 @@
-const textAnim = {
-    old: {
-        name: 'textShrink',
-        duration: '1s',
-        easing: 'ease-in',
-        fillMode: 'forwards',
-    },
-    new: {
-        name: 'textGrow',
-        duration: '1s',
-        easing: 'ease-out',
-        fillMode: 'backwards',
-    }
-};
-
-export const textSpacing = {
-    forwards: textAnim,
-    backwards: textAnim,
-};
-
 const slideAnim = (duration: number, toRight: boolean) => {
     return {
         forwards: {
