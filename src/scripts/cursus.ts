d4a35c12347943399d88e4dc336a60754a8c4e18
Antoine Blumenroeder <z124008@shibaura-it.ac.jp>
2024-05-26 16:12:40 +0900

    better cursus

--- src/scripts/cursus.ts
+++ src/scripts/cursus.ts
@@ -2,7 +2,8 @@ document.addEventListener('astro:page-load', () => {
     if (!document.querySelector('.cursus'))
         return;
 
-    if (window.matchMedia('screen and (min-width: 1000px)').matches) {
+    const matches = () => window.matchMedia('screen and (min-width: 1000px)').matches;
+    if (matches()) {
         gsap.to('#first-section', {
             scrollTrigger: {
                 trigger: '#first-section',
@@ -27,6 +28,10 @@ document.addEventListener('astro:page-load', () => {
     }
 
     const experiences = gsap.utils.toArray('.experience');
+    const curve = document.querySelector('.curve') as SVGPathElement;
+    const length = curve.getTotalLength();
+    curve.style.strokeDasharray = `${length}px`;
+    curve.style.strokeDashoffset = `${length}px`;
     experiences.forEach((experience, index) => {
         const div = experience as HTMLElement;
         const circle = div.querySelector('.circle') as HTMLElement;
@@ -41,14 +46,24 @@ document.addEventListener('astro:page-load', () => {
             }
         });
 
+        if (index !== 0)
+            tl.fromTo(curve, {
+                strokeDasharray: length,
+                strokeDashoffset: length * (experiences.length - index + 1)/experiences.length,
+            }, {
+                strokeDashoffset: (experiences.length - index) * (length / experiences.length),
+                duration: 0.8,
+                immediateRender: false,
+                ease: 'power1.inOut',
+            });
         tl.from(circle, {
             height: 0,
             width: 0,
-            duration: 0.3,
+            duration: 0.2,
         })
         .from(line, {
             width: 0,
-            duration: 0.5,
+            duration: 0.3,
         }, '>')
         .from(content, {
             x: 20,
