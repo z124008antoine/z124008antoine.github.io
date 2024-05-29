69af8de0cf85ae28bcfdeb384e45e0e9ff78b6de
Antoine Blumenroeder <z124008@shibaura-it.ac.jp>
2024-05-29 15:10:04 +0900

    much better

--- src/scripts/cursus.ts
+++ src/scripts/cursus.ts
@@ -3,29 +3,35 @@ document.addEventListener('astro:page-load', () => {
         return;
 
     const matches = () => window.matchMedia('screen and (min-width: 1000px)').matches;
-    if (matches()) {
-        gsap.to('#first-section', {
-            scrollTrigger: {
-                trigger: '#first-section',
-                start: 'top top',
-                end: 'top -100%',
-                pin: true,
-                pinSpacing: false,
-            }
-        });
+    let anims: gsap.core.Tween[] = [];
+    const update = () => {
+        // make sure to add the animations if the screen gets bigger
+        if (matches() && anims.length === 0) {
+            anims.push(gsap.to('#first-section', {
+                scrollTrigger: {
+                    trigger: '#first-section',
+                    start: 'top top',
+                    end: 'top -100%',
+                    pin: true,
+                    pinSpacing: false,
+                }
+            }));
 
-        gsap.to('.cursus', {
-            scrollTrigger: {
-                trigger: '#second-section',
-                start: 'top top',
-                end: 'bottom top',
-                pin: true,
-                scrub: true,
-            },
-            ease: 'none',
-            y: '-100%'
-        });
+            anims.push(gsap.to('.cursus', {
+                scrollTrigger: {
+                    trigger: '#second-section',
+                    start: 'top top',
+                    end: 'bottom top',
+                    pin: true,
+                    scrub: true,
+                },
+                ease: 'none',
+                y: '-100%'
+            }));
+        }
     }
+    update();
+    window.addEventListener('resize', update);
 
     const experiences = gsap.utils.toArray('.experience');
     const curve = document.querySelector('.curve') as SVGPathElement;
@@ -52,7 +58,7 @@ document.addEventListener('astro:page-load', () => {
                 strokeDashoffset: length * (experiences.length - index + 1)/experiences.length,
             }, {
                 strokeDashoffset: (experiences.length - index) * (length / experiences.length),
-                duration: 0.8,
+                duration: 0.6,
                 immediateRender: false,
                 ease: 'power1.inOut',
             });
@@ -68,7 +74,7 @@ document.addEventListener('astro:page-load', () => {
         .from(content, {
             x: 20,
             opacity: 0,
-            duration: 0.5,
+            duration: 0.3,
         }, '<');
     });
 });
